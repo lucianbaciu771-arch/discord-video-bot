7ab5585827f160f7aa62e27cdf001bb656f74b96
lucianbaciu771-arch <lucianbaciu771@gmail.com>
2026-03-14 18:22:44 +0200

    bot.py

--- bot.py
+++ bot.py
@@ -1,64 +1,54 @@
 import discord
 import os
 import random
+import gdown
 from discord.ext import tasks
 from datetime import time
 
 TOKEN = os.getenv("TOKEN")
 CHANNEL_ID = 1475585161876738272
 
-VIDEO_FOLDER = "videos"
-POSTED_FILE = "posted_videos.txt"
+DRIVE_FOLDER = "https://drive.google.com/drive/folders/1Xry7zBispB-ZbNS2lx88bqMh-oCcuxdr?usp=sharing"
+
+DOWNLOAD_FOLDER = "videos"
 
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
 
 
-def get_posted():
-    if not os.path.exists(POSTED_FILE):
-        return []
-    with open(POSTED_FILE, "r") as f:
-        return f.read().splitlines()
+async def download_videos():
 
+    if not os.path.exists(DOWNLOAD_FOLDER):
+        os.makedirs(DOWNLOAD_FOLDER)
 
-def save_posted(video):
-    with open(POSTED_FILE, "a") as f:
-        f.write(video + "\n")
+    gdown.download_folder(DRIVE_FOLDER, output=DOWNLOAD_FOLDER, quiet=True)
 
 
 async def post_videos():
 
     channel = client.get_channel(CHANNEL_ID)
 
-    videos = os.listdir(VIDEO_FOLDER)
-    posted = get_posted()
+    videos = os.listdir(DOWNLOAD_FOLDER)
 
-    available = [v for v in videos if v not in posted]
-
-    if len(available) < 6:
-        print("Not enough new videos")
-        return
-
-    selected = random.sample(available, 6)
+    selected = random.sample(videos, 6)
 
     for video in selected:
 
-        await channel.send(file=discord.File(f"{VIDEO_FOLDER}/{video}"))
-
-        save_posted(video)
-
-        await discord.utils.sleep_until(discord.utils.utcnow())
+        await channel.send(file=discord.File(f"{DOWNLOAD_FOLDER}/{video}"))
 
 
 @tasks.loop(time=time(hour=20, minute=0))
-async def daily_videos():
+async def daily_post():
+
+    await download_videos()
     await post_videos()
 
 
 @client.event
 async def on_ready():
-    print(f"Bot online: {client.user}")
-    daily_videos.start()
+
+    print("Bot online")
+    daily_post.start()
 
 
 client.run(TOKEN)
